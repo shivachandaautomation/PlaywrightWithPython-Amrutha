b6712f31b75b40c28369a89d81fbc3adf10041b0
chrj782 <chrj782@gmail.com>
2026-06-02 22:58:44 +0530

    Add Day-5 PythonVariables example

---
+++ Day-5/PythonVariables.py
@@ -0,0 +1,36 @@
+# age = '30'
+
+# print((age))
+
+# name = "shiva"
+# print(type(name))
+
+# Operators
+# +, -, *, /, %, **, //, =, +=, -=, *=, /=, %=, **=, //=
+# Comparison Operators
+# print(8!=23)
+
+# name = input("Enter your name: ")
+# print(name)
+# print("Hello, " + name + "! Welcome to Python programming.")
+
+# Strings
+
+country = "India"
+city = 'Bangalore'
+
+# String Indexing
+
+# x = "Hello, World!"
+# print(x[0])  # Output: H
+# print(x[7])  # Output: W
+# print(x[-1]) # Output: !
+# print(x[-6]) # Output: W
+
+# String slicing
+
+text = "Python Programming"
+print(text[0:6])  # Output: Python
+print(text[7:18]) # Output: Programming 
+
+
